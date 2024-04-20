c1a13a4dc73b14a0f58c5362dd381d1b104e3894
parkdaechul <eocjf4701@gmail.com>
2024-04-20 23:33:51 +0900

    초기 날씨정보 가져오는 값 셋팅 전까지 로딩스피너 적용, 결과값 소수점 두번째 자리까지 허용

--- src/App.js
+++ src/App.js
@@ -55,6 +55,7 @@ function App() {
 
 
   useEffect(() => {
+    setLoading(true);
     getCurrentLocation();
   }, []);
 
--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -5,7 +5,7 @@ const WeatherBox = ({ weather }) => {
   return (
     <div className='weather-box'>
       <div>{weather?.name}</div>
-      <h2>{weather?.main.temp}C / {(weather?.main.temp * 9 / 5) + 32}°F</h2>
+      <h2>{weather?.main.temp}C / {((weather?.main.temp * 9 / 5) + 32).toFixed(2)}°F</h2>
       <h3>{weather?.weather[0].description}</h3>
     </div>
   )
